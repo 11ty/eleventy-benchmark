77d25d1aaea440890ee99af8fb6b51c9407c0063
francisfuzz <15894826+francisfuzz@users.noreply.github.com>
2020-02-24 20:20:30 -0800

    let bench.sh accept a version of 11ty to install

--- bench.sh
+++ bench.sh
@@ -1,8 +1,15 @@
 TEMPLATE_FILES=1000
 RUNS=10
 
- # Also use a local version like "file:../eleventy"
-VERSIONS=("@11ty/eleventy@0.5.4" "@11ty/eleventy@0.6.0")
+if [ -n "$1" ]
+then
+	echo "Version detected: $1"
+else
+	echo "No version detected, exiting."
+	exit 1
+fi
+
+ELEVENTY_VERSION=$1
 
 # LANGS=("liquid" "njk" "md" "11ty.js")
 LANGS=("liquid" "njk" "md")
@@ -16,62 +23,60 @@ echo "Eleventy Benchmark (Node $nodeVersion, $TEMPLATE_FILES templates each)"
 BASELINEMEDIAN=()
 BASELINEPERTEMPLATE=()
 RESULTS=()
-for npmVersion in "${VERSIONS[@]}"; do
-	echo "$LINESEP"
-	printf "Running npm install $npmVersion\r"
-	npm install $npmVersion > /dev/null 2>&1
 
-	rm -rf _site
+echo "$LINESEP"
+printf "Running npm install $ELEVENTY_VERSION\r"
+npm install @11ty/eleventy@$ELEVENTY_VERSION > /dev/null 2>&1
 
-	eleventyVersion=`npx eleventy --version`
-	echo "Eleventy $eleventyVersion                                        "
-	echo "$LINESEP"
+rm -rf _site
 
-	for (( i=0; i<${#LANGS[@]}; i++ )); do
-		if [[ ${#RESULTS[@]} < $i+1 ]]; then
-			RESULTS+=("")
-		fi
+eleventyVersion=`npx eleventy --version`
+echo "Eleventy $eleventyVersion                                        "
+echo "$LINESEP"
 
-		rm -rf "${LANGS[$i]}/page/"
-		printf "Creating template files…\r"
-		./make-${LANGS[$i]}-files.sh $TEMPLATE_FILES
-		printf "                        \r"
-		printf ".${LANGS[$i]}: "
+for (( i=0; i<${#LANGS[@]}; i++ )); do
+	if [[ ${#RESULTS[@]} < $i+1 ]]; then
+		RESULTS+=("")
+	fi
 
-		TIMES=""
-		for ((j=1; j<=$RUNS; j++)); do
-			eleventyTime=`npx eleventy --quiet --formats=${LANGS[$i]}`
-			printf "."
+	rm -rf "${LANGS[$i]}/page/"
+	printf "Creating template files…\r"
+	./make-${LANGS[$i]}-files.sh $TEMPLATE_FILES
+	printf "                        \r"
+	printf ".${LANGS[$i]}: "
 
-			# Extract the total time
-			# Expected Format: Copied 1 item and Processed 0 files in 0.14 seconds
-			eleventyTimeNumber=`echo $eleventyTime | awk '/a/ {print $5}'`
-			TIMES="$TIMES$eleventyTimeNumber\n"
-		done
+	TIMES=""
+	for ((j=1; j<=$RUNS; j++)); do
+		eleventyTime=`npx eleventy --quiet --formats=${LANGS[$i]}`
+		printf "."
 
-		printf " $RUNS runs.\n"
-
-		median=`printf $TIMES | datamash median 1`
-		perTemplate=`echo "$median * 1000 / $TEMPLATE_FILES" | bc`
-
-		printf "* Median: $median seconds"
-		if [[ ${#BASELINEMEDIAN[@]} < $i+1 ]]; then
-			BASELINEMEDIAN+=($median)
-			echo ""
-		else
-			baselineCompare=`echo "$median * 100 / ${BASELINEMEDIAN[$i]} - 100" | bc`
-			echo " (${baselineCompare}%)"
-		fi
-
-		printf "* Median per template: $perTemplate ms"
-		if [[ ${#BASELINEPERTEMPLATE[@]} < $i+1 ]]; then
-			BASELINEPERTEMPLATE+=($perTemplate)
-			echo ""
-		else
-			baselinePerTemplateCompare=`echo "$perTemplate * 100 / ${BASELINEPERTEMPLATE[$i]} - 100" | bc`
-			echo " (${baselinePerTemplateCompare}%)"
-		fi
-		echo ""
+		# Extract the total time
+		# Expected Format: Copied 1 item and Processed 0 files in 0.14 seconds
+		eleventyTimeNumber=`echo $eleventyTime | awk '/a/ {print $5}'`
+		TIMES="$TIMES$eleventyTimeNumber\n"
 	done
-done
 
+	printf " $RUNS runs.\n"
+
+	median=`printf $TIMES | datamash median 1`
+	perTemplate=`echo "$median * 1000 / $TEMPLATE_FILES" | bc`
+
+	printf "* Median: $median seconds"
+	if [[ ${#BASELINEMEDIAN[@]} < $i+1 ]]; then
+		BASELINEMEDIAN+=($median)
+		echo ""
+	else
+		baselineCompare=`echo "$median * 100 / ${BASELINEMEDIAN[$i]} - 100" | bc`
+		echo " (${baselineCompare}%)"
+	fi
+
+	printf "* Median per template: $perTemplate ms"
+	if [[ ${#BASELINEPERTEMPLATE[@]} < $i+1 ]]; then
+		BASELINEPERTEMPLATE+=($perTemplate)
+		echo ""
+	else
+		baselinePerTemplateCompare=`echo "$perTemplate * 100 / ${BASELINEPERTEMPLATE[$i]} - 100" | bc`
+		echo " (${baselinePerTemplateCompare}%)"
+	fi
+	echo ""
+done
